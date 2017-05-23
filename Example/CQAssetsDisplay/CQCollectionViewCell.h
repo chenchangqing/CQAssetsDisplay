ea3732e90f7a6698001ea4f5ede46bb08a3c1c6b
陈长青 <chenchangqing@chenchangqingdeiMac.local>
2017-05-23 16:08:57 +0800

    增加相册

---
+++ Example/CQAssetsDisplay/CQCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CQCollectionViewCell.h
+//  CQAssetsDisplay
+//
+//  Created by 陈长青 on 2017/5/23.
+//  Copyright © 2017年 chenchangqing198@126.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Example/CQAssetsDisplay/CQCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CQCollectionViewCell.m
+//  CQAssetsDisplay
+//
+//  Created by 陈长青 on 2017/5/23.
+//  Copyright © 2017年 chenchangqing198@126.com. All rights reserved.
+//
+
+#import "CQCollectionViewCell.h"
+
+@implementation CQCollectionViewCell
+
+@end
