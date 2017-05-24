8cf00f3a200ebf8f480ac2fcc632532b705fff95
陈长青 <chenchangqing@chenchangqingdeiMac.local>
2017-05-24 14:06:05 +0800

    * dev

--- CQAssetsDisplay/Classes/CQAssetsDisplayController.m
+++ CQAssetsDisplay/Classes/CQAssetsDisplayController.m
@@ -585,7 +585,7 @@ typedef NSMutableDictionary<NSString *, UIView *> LeftPlaceholdViewDic;
         UIView *contentView = [UIView new];
         contentView.userInteractionEnabled = NO;
         contentView.translatesAutoresizingMaskIntoConstraints = NO;
-        contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:1];
+        contentView.backgroundColor = [UIColor clearColor];
         [_scrollViewContentView addSubview:contentView];
         item.contentView = contentView;
         
